157b4fdd231087c6bee3967e0d68c58bb4e21ae8
Heriberto Amezcua <86883781+hoxuro@users.noreply.github.com>
2023-03-18 13:30:28 +0100

    Update ReadKBData.java

--- METHODS/ReadKBData.java
+++ METHODS/ReadKBData.java
@@ -5,6 +5,7 @@
 package methods;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * Class with different methods to facilitate data keyboard entry.
@@ -16,7 +17,7 @@ import java.util.Scanner;
 public class ReadKBData {
 
     /**
-     * Method that asks the user to enter an integer type number and also
+     * Method that asks the user to enter an integer data type and also
      * validates it.
      *
      * @param message the message that we want to display on the screen before
@@ -25,13 +26,15 @@ public class ReadKBData {
      */
     public static int enterInt(String message) {
         Scanner input = new Scanner(System.in);
-        System.out.println(message);
+
+        if (!Pattern.matches("^\\s*$", message)) {
+            System.out.println(message);
+        }
 
         if (!input.hasNextInt()) {
             do {
-                System.out.println("Please enter the data correctly!");
+                System.err.println("Please, enter the data correctly");
                 input.nextLine();
-                System.out.println(message);
 
             } while (!input.hasNextInt());
         }
@@ -40,7 +43,7 @@ public class ReadKBData {
     }
 
     /**
-     * Method that asks the user to enter an String type data and also validates
+     * Method that asks the user to enter an String data type and also validates
      * it.
      *
      * @param message the message that we want to display on the screen before
@@ -51,13 +54,14 @@ public class ReadKBData {
         Scanner input = new Scanner(System.in);
 
         //remember that you can change the delimiter
-        System.out.println(message);
+        if (!Pattern.matches("^\\s*$", message)) {
+            System.out.println(message);
+        }
 
         if (!input.hasNext()) {
             do {
-                System.out.println("Please enter the data correctly!");
+                System.err.println("Please, enter the data correctly");
                 input.nextLine();
-                System.out.println(message);
 
             } while (!input.hasNext());
         }
@@ -66,25 +70,82 @@ public class ReadKBData {
     }
 
     /**
-     * Method that asks the user to enter a double type number and also
-     * validates it.
+     * Method that asks the user to enter a String data type and also validates
+     * it.
+     *
+     * @param message the message we want to display on the screen requesting
+     * the String.
+     * @return a valid string.
+     */
+    public static String enterStringNextLine(String message) {
+        Scanner input = new Scanner(System.in);
+
+        //remember that you can change the delimiter
+        if (!Pattern.matches("^\\s*$", message)) {
+            System.out.println(message);
+        }
+        if (!input.hasNextLine()) {
+            do {
+                System.err.println("Please, enter the data correctly");
+                input.nextLine();
+
+            } while (!input.hasNextLine());
+        }
+
+        return input.nextLine();
+    }
+
+    /**
+     * Method that asks the user to enter a double data type and also validates
+     * it.
      *
      * @param message the message that we want to display on the screen before
      * requesting the String.
      * @return a valid double.
      */
     public static double enterDouble(String message) {
+
         Scanner input = new Scanner(System.in);
-        System.out.println(message);
+
+        if (!Pattern.matches("^\\s*$", message)) {
+            System.out.println(message);
+        }
 
         if (!input.hasNextDouble()) {
             do {
                 System.out.println("Please enter the data correctly!");
                 input.nextLine();
+
             } while (!input.hasNextDouble());
         }
 
         return input.nextDouble();
+    }
+
+    /**
+     * Method that asks the user to enter a Boolean data type and also validates
+     * it.
+     *
+     * @param message the message we want to display on the screen requesting
+     * the String.
+     * @return a valid boolean.
+     */
+    public static boolean enterBoolean(String message) {
+        Scanner input = new Scanner(System.in);
+
+        //remember that you can change the delimiter
+        System.out.println(message);
+
+        if (!input.hasNextBoolean()) {
+            do {
+                System.err.println("Please, enter the data correctly");
+                input.nextLine();
+                System.out.println(message);
+
+            } while (!input.hasNextBoolean());
+        }
+
+        return input.nextBoolean();
     }
 
 }
