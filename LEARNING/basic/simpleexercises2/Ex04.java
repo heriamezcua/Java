faa4337376b26e6b9bba8222b4e89379f68d4bf8
Heriberto Amezcua <86883781+hoxuro@users.noreply.github.com>
2024-11-11 09:14:47 +0100

    Refactoring repo: simpleexercises2: 1 - 5

---
+++ LEARNING/basic/simpleexercises2/Ex04.java
@@ -0,0 +1,29 @@
+package basic.simpleexercises2;
+
+import basic.utilis.ReadKBData;
+
+/*
+ * Program to guess the random number from 1 to 10
+ */
+public class Ex04 {
+
+	public static void main(String[] args) {
+		int answer = 0;
+		int randomNum = (int) (Math.random() * 10) + 1;
+
+		System.out.println("Guess the number between 0-10:");
+		do {
+			answer = ReadKBData.enterInt("Enter a number:");
+			if (answer != randomNum) {
+				if (answer < randomNum) {
+					System.out.println("The number is greater than " + answer);
+				} else {
+					System.out.println("The number is less than " + answer);
+				}
+			}
+		} while (answer != randomNum);
+
+		System.out.println("Correct! The number was " + randomNum);
+	}
+
+}
