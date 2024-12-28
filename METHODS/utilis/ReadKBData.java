3f30b64fd2dc5826db5a929e1c9e22295615cc3f
Heriberto Amezcua <86883781+hoxuro@users.noreply.github.com>
2024-12-28 12:45:39 +0100

    enterValidInput in ReadKBData

--- METHODS/utilis/ReadKBData.java
+++ METHODS/utilis/ReadKBData.java
@@ -3,6 +3,8 @@ package basic.utilis;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+import pente.utilis.ReadKBData;
+
 /**
  * Class with different methods to facilitate data keyboard entry.
  *
@@ -12,146 +14,161 @@ import java.util.regex.Pattern;
  */
 public class ReadKBData {
 
-    /**
-     * Method that asks the user to enter an integer data type and also
-     * validates it.
-     *
-     * @param message the message that we want to display on the screen before
-     * requesting the number.
-     * @return a valid integer.
-     */
-    public static int enterInt(String message) {
-        Scanner input = new Scanner(System.in);
+	/**
+	 * Method that asks the user to enter an integer data type and also validates
+	 * it.
+	 *
+	 * @param message the message that we want to display on the screen before
+	 *                requesting the number.
+	 * @return a valid integer.
+	 */
+	public static int enterInt(String message) {
+		Scanner input = new Scanner(System.in);
 
-        if (!Pattern.matches("^\\s*$", message)) {
-            System.out.println(message);
-        }
+		if (!Pattern.matches("^\\s*$", message)) {
+			System.out.println(message);
+		}
 
-        if (!input.hasNextInt()) {
-            do {
-                System.err.println("Please, enter the data correctly");
-                input.nextLine();
+		if (!input.hasNextInt()) {
+			do {
+				System.err.println("Please, enter the data correctly");
+				input.nextLine();
 
-            } while (!input.hasNextInt());
-        }
-       
-        input.close();
+			} while (!input.hasNextInt());
+		}
 
-        return input.nextInt();
-    }
+		input.close();
 
-    /**
-     * Method that asks the user to enter an String data type and also validates
-     * it.
-     *
-     * @param message the message that we want to display on the screen before
-     * requesting the String.
-     * @return a valid String.
-     */
-    public static String enterString(String message) {
-        Scanner input = new Scanner(System.in);
+		return input.nextInt();
+	}
 
-        //remember that you can change the delimiter
-        if (!Pattern.matches("^\\s*$", message)) {
-            System.out.println(message);
-        }
+	/**
+	 * Method that asks the user to enter an String data type and also validates it.
+	 *
+	 * @param message the message that we want to display on the screen before
+	 *                requesting the String.
+	 * @return a valid String.
+	 */
+	public static String enterString(String message) {
+		Scanner input = new Scanner(System.in);
 
-        if (!input.hasNext()) {
-            do {
-                System.err.println("Please, enter the data correctly");
-                input.nextLine();
+		// remember that you can change the delimiter
+		if (!Pattern.matches("^\\s*$", message)) {
+			System.out.println(message);
+		}
 
-            } while (!input.hasNext());
-        }
-        
-        input.close();
+		if (!input.hasNext()) {
+			do {
+				System.err.println("Please, enter the data correctly");
+				input.nextLine();
 
-        return input.next();
-    }
+			} while (!input.hasNext());
+		}
 
-    /**
-     * Method that asks the user to enter a String data type and also validates
-     * it.
-     *
-     * @param message the message we want to display on the screen requesting
-     * the String.
-     * @return a valid string.
-     */
-    public static String enterStringNextLine(String message) {
-        Scanner input = new Scanner(System.in);
+		input.close();
 
-        //remember that you can change the delimiter
-        if (!Pattern.matches("^\\s*$", message)) {
-            System.out.println(message);
-        }
-        if (!input.hasNextLine()) {
-            do {
-                System.err.println("Please, enter the data correctly");
-                input.nextLine();
+		return input.next();
+	}
 
-            } while (!input.hasNextLine());
-        }
-        
-        input.close();
+	/**
+	 * Method that asks the user to enter a String data type and also validates it.
+	 *
+	 * @param message the message we want to display on the screen requesting the
+	 *                String.
+	 * @return a valid string.
+	 */
+	public static String enterStringNextLine(String message) {
+		Scanner input = new Scanner(System.in);
 
-        return input.nextLine();
-    }
+		// remember that you can change the delimiter
+		if (!Pattern.matches("^\\s*$", message)) {
+			System.out.println(message);
+		}
+		if (!input.hasNextLine()) {
+			do {
+				System.err.println("Please, enter the data correctly");
+				input.nextLine();
 
-    /**
-     * Method that asks the user to enter a double data type and also validates
-     * it.
-     *
-     * @param message the message that we want to display on the screen before
-     * requesting the String.
-     * @return a valid double.
-     */
-    public static double enterDouble(String message) {
+			} while (!input.hasNextLine());
+		}
 
-        Scanner input = new Scanner(System.in);
+		input.close();
 
-        if (!Pattern.matches("^\\s*$", message)) {
-            System.out.println(message);
-        }
+		return input.nextLine();
+	}
 
-        if (!input.hasNextDouble()) {
-            do {
-                System.out.println("Please enter the data correctly!");
-                input.nextLine();
+	/**
+	 * Method that asks the user to enter a double data type and also validates it.
+	 *
+	 * @param message the message that we want to display on the screen before
+	 *                requesting the String.
+	 * @return a valid double.
+	 */
+	public static double enterDouble(String message) {
 
-            } while (!input.hasNextDouble());
-        }
-        
-        input.close();
+		Scanner input = new Scanner(System.in);
 
-        return input.nextDouble();
-    }
+		if (!Pattern.matches("^\\s*$", message)) {
+			System.out.println(message);
+		}
 
-    /**
-     * Method that asks the user to enter a Boolean data type and also validates
-     * it.
-     *
-     * @param message the message we want to display on the screen requesting
-     * the String.
-     * @return a valid boolean.
-     */
-    public static boolean enterBoolean(String message) {
-        Scanner input = new Scanner(System.in);
+		if (!input.hasNextDouble()) {
+			do {
+				System.out.println("Please enter the data correctly!");
+				input.nextLine();
 
-        //remember that you can change the delimiter
-        System.out.println(message);
+			} while (!input.hasNextDouble());
+		}
 
-        if (!input.hasNextBoolean()) {
-            do {
-                System.err.println("Please, enter the data correctly");
-                input.nextLine();
-                System.out.println(message);
+		input.close();
 
-            } while (!input.hasNextBoolean());
-        }
-        
-        input.close();
+		return input.nextDouble();
+	}
 
-        return input.nextBoolean();
-    }
+	/**
+	 * Method that asks the user to enter a Boolean data type and also validates it.
+	 *
+	 * @param message the message we want to display on the screen requesting the
+	 *                String.
+	 * @return a valid boolean.
+	 */
+	public static boolean enterBoolean(String message) {
+		Scanner input = new Scanner(System.in);
+
+		// remember that you can change the delimiter
+		System.out.println(message);
+
+		if (!input.hasNextBoolean()) {
+			do {
+				System.err.println("Please, enter the data correctly");
+				input.nextLine();
+				System.out.println(message);
+
+			} while (!input.hasNextBoolean());
+		}
+
+		input.close();
+
+		return input.nextBoolean();
+	}
+
+	/**
+	 * Prompts the user to enter a valid integer input within a specified range.
+	 * 
+	 * @param message The message displayed to the user prompting them for input.
+	 * @param min     The minimum allowable input value.
+	 * @param max     The maximum allowable input value.
+	 * @return A valid integer input within the specified range.
+	 */
+	public static int askValidInput(String message, int min, int max) {
+		int value;
+		do {
+			value = ReadKBData.enterInt(message);
+			if (value < min || value > max) {
+				System.err.println("Input must be between " + min + " and " + max);
+			}
+		} while (value < min || value > max);
+		return value;
+	}
 
 }
